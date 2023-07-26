e4d62216ffa604a08b48427c3018fbe9b73507c9
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-07-26 19:20:09 -0400

    comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Post, User, Comment} = require('../models')
+const {Post, User, Comment} = require('../models') // gabs the models from post, user, and comment
 
 router.get("/", (req, res) => {
     Post.findAll({
