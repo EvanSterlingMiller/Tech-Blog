a789ae3ec89debe72cfcc36c116a53ebd9c05216
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-07-29 21:04:57 -0400

    comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,19 +30,19 @@ router.get("/", (req, res) => { // get route for root
             },
         ],
     })
-    .then(dbPostData => {
+    .then(dbPostData => { //after fetching the data the page renders the homepage
         const posts = dbPostData.map(post => post.get({ plain:true}))
         res.render('homepage', {
         posts,
-        loggedIn: req.session.loggedIn,
+        loggedIn: req.session.loggedIn, // flags for logged in
         })
     })
-    .catch(err => {
+    .catch(err => { // sends error if something goes wrong
         console.log(err)
         res.statusC(500).json(err)
     })
 })
-router.get('/post/:id', (req, res) => {
+router.get('/post/:id', (req, res) => { // view a single post by its id
     Post.findOne({
         where: {
             id: req.params.id,
@@ -74,35 +74,35 @@ router.get('/post/:id', (req, res) => {
             },
         ],
     })
-    .then(dbPostData => {
+    .then(dbPostData => { // if the post is not found by the id then it sends a 404 error
         if (!dbPostData) {
             res.status(404).json({ message: 'No post was found with this id' });
             return;
         }
-        const post = dbPostData.get({ plain: true });
+        const post = dbPostData.get({ plain: true }); // if the post is found then it will render the single-post view
         res.render('single-post', {
             post,
             loggedIn: req.session.loggedIn,
         });
     })
-    .catch(err => {
+    .catch(err => { // if there is an error in the database sde it will send a 500 error
         console.log(err);
         res.status(500).json(err);
     });
 });
 
 
-router.get('/login', (req, res) => {
+router.get('/login', (req, res) => { //renders the login page
     if (req.session.loggedIn) {
-        res.redirect('/')
+        res.redirect('/') // redirects to the root page once the user logs in
         return
     }
     res.render('login')
 })
 
-router.get('/signup', (req, res) => {
+router.get('/signup', (req, res) => { // signs the user up
     if (req.session.loggedIn) {
-        res.redirect('/')
+        res.redirect('/') //redirects to the login page
         return
     }
     res.render('signup')
